9fb656ffe21b2256d73626f1ad92b3fed3085548
Bilal <86686062+Bilal-A-G@users.noreply.github.com>
2024-06-19 20:28:53 -0400

    Added wall jumping, fully finished character controller

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,27 +8,56 @@ public class PlayerMovement : AMovement
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpHeight;
     [SerializeField] private float gravityStrength;
+    [SerializeField] private float wallSlideSpeed;
 
     [SerializeField] private Transform groundCheck;
+    [SerializeField] private List<Transform> wallChecks;
+    [SerializeField] private float wallCheckRadius;
     [SerializeField] private float groundCheckRadius;
     [SerializeField] private LayerMask groundLayer;
 
     [SerializeField] private Rigidbody2D rigidBody;
 
     private float m_gravityPull;
-    private bool m_isGrounded;
+    private GameObject m_wallLastFrame;
+    private bool m_allowedToWallJump;
 
     public override void Move(Vector2 moveDirection)
     {
         RaycastHit2D hit = Physics2D.CircleCast(groundCheck.position, groundCheckRadius, Vector2.zero, 0.0f, groundLayer);
-        m_isGrounded = hit;
-        if (hit && Vector2.Dot(hit.normal, Vector2.up) <= 0.3f)
-            m_isGrounded = false;
+        bool isGrounded = hit && Vector2.Dot(hit.normal, Vector2.up) > 0.3f;
 
-        rigidBody.AddForce((Vector2.right * moveDirection.x * moveSpeed + -Vector2.up * m_gravityPull) * Time.deltaTime);
-        if (m_isGrounded)
-            rigidBody.AddForce(Vector2.up * moveDirection.y * jumpHeight * (hit ? 1 : 0), ForceMode2D.Force);
+        bool isOnWall = false;
+        GameObject currentWall = null;
+        for (int i = 0; i < wallChecks.Count; i++)
+        {
+            RaycastHit2D wallHit = Physics2D.CircleCast(wallChecks[i].position, groundCheckRadius, Vector2.zero, 0.0f, groundLayer);
+            if (wallHit)
+            {
+                isOnWall = true;
+                currentWall = wallHit.transform.gameObject;
+                break;
+            }
+        }
 
-        m_gravityPull = hit ? 0 : m_gravityPull + Time.deltaTime * gravityStrength;
+        m_allowedToWallJump = !((m_wallLastFrame == currentWall) && (currentWall != null && m_wallLastFrame != null));
+
+        float gravityModifier = m_gravityPull;
+        if (isOnWall && m_allowedToWallJump)
+            gravityModifier = -wallSlideSpeed;
+
+        rigidBody.AddForce((Vector2.right * moveDirection.x * moveSpeed + -Vector2.up * gravityModifier) * Time.deltaTime);
+        m_gravityPull = ((isGrounded || isOnWall) && m_allowedToWallJump) ? 0 : m_gravityPull + Time.deltaTime * gravityStrength;
+
+        if (moveDirection.y == 0)
+            return;
+
+        if (!m_allowedToWallJump)
+            return;
+        if (isGrounded || isOnWall)
+            rigidBody.AddForce(Vector2.up * moveDirection.y * jumpHeight, ForceMode2D.Force);
+
+        m_wallLastFrame = currentWall;
+
     }
 }
